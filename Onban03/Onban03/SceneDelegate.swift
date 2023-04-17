d5c09a96f782de86b929922b9421657dee62877d
Hyejeong Park <effie.thedev@gmail.com>
2023-04-17 15:01:00 +0900

    Init Xcode Project
    - iOS 프로젝트 생성

---
+++ Onban03/Onban03/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  Onban03
+//
+//  Created by Effie on 2023/04/17.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+}
+
