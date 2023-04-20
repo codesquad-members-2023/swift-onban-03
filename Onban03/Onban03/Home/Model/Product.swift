e82c38dd97d3af64afca27fccfe37ad2a094981a
Hyejeong Park <effie.thedev@gmail.com>
2023-04-20 14:31:20 +0900

    Add Product
    - Product 구조체를 추가하고 모델 그룹핑

---
+++ Onban03/Onban03/Home/Model/Product.swift
@@ -0,0 +1,24 @@
+//
+//  Product.swift
+//  Onban03
+//
+//  Created by Effie on 2023/04/20.
+//
+
+import Foundation
+
+struct Product {
+  var category: ProductCategory
+  
+  var thumbnailImageUrl: URL
+  
+  var name: String
+  
+  var briefDescription: String
+  
+  var tagPrice: Int
+  
+  var discountedPrice: Int
+  
+  var tags: [Tag]
+}
