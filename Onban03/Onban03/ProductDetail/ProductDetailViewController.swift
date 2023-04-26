8a9d7b52c6211ea11b2501337e44ae1f2942287d
Hyejeong Park <effie.thedev@gmail.com>
2023-04-26 16:30:31 +0900

    Add tableView to product detail VC
    - 상품상세 뷰 컨트롤러에 table view 추가

--- Onban03/Onban03/ProductDetail/ProductDetailViewController.swift
+++ Onban03/Onban03/ProductDetail/ProductDetailViewController.swift
@@ -7,4 +7,30 @@
 
 import UIKit
 
-class ProductDetailViewController: UIViewController { }
+class ProductDetailViewController: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.dataSource = self
+  }
+}
+
+extension ProductDetailViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 6
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    switch section {
+    case 0...4: return 1
+    case 5: return 3
+    default: return 0
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+  }
+}
