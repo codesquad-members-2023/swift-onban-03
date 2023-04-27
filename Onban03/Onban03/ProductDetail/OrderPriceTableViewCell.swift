11f331b51e1e55ef2e0441b48d3c8b9be9f269d6
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-04-27 15:22:34 +0900

    Add OrderPriceTableViewCell
    - 상품 상세 뷰에서 주문 가격을 표시하는 셀 추가

---
+++ Onban03/Onban03/ProductDetail/OrderPriceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrderPriceTableViewCell.swift
+//  Onban03
+//
+//  Created by 에디 on 2023/04/27.
+//
+
+import UIKit
+
+class OrderPriceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
